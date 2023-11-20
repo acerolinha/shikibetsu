4c14d27e9dfae6645cbdac4b2fe11b702dffb491
Felipe Cardoso <agn0stkp@gmail.com>
2023-11-20 20:25:21 -0300

    refactor: sort args ascending

--- src/args.rs
+++ src/args.rs
@@ -14,20 +14,8 @@ pub struct Args {
     #[arg(default_value = ".")]
     pub path: std::path::PathBuf,
 
-    #[arg(short = 'e', long = "emoji", default_value = "false")]
-    pub show_emoji_icon: bool,
-
     #[arg(short = 'a', long = "all", default_value = "false")]
     pub show_hidden: bool,
-
-    #[arg(short = 'r', long = "reverse", default_value = "false")]
-    pub reverse: bool,
-
-    #[arg(short = 'S', long = "size", default_value = "false")]
-    pub show_size: bool,
-
-    #[arg(short = 'm', long = "modified", default_value = "false")]
-    pub show_modified_ts: bool,
 
     #[arg(short = 'c', long = "created", default_value = "false")]
     pub show_created_ts: bool,
@@ -35,8 +23,20 @@ pub struct Args {
     #[arg(short = 'd', long = "dirs", default_value = "false")]
     pub show_only_dirs: bool,
 
+    #[arg(short = 'e', long = "emoji", default_value = "false")]
+    pub show_emoji_icon: bool,
+
     #[arg(short = 'f', long = "files", default_value = "false")]
     pub show_only_files: bool,
+
+    #[arg(short = 'm', long = "modified", default_value = "false")]
+    pub show_modified_ts: bool,
+
+    #[arg(short = 'r', long = "reverse", default_value = "false")]
+    pub reverse: bool,
+
+    #[arg(short = 'S', long = "size", default_value = "false")]
+    pub show_size: bool,
 
     #[arg(short = 's', long = "sort", default_value_t = SortKey::Name)]
     pub sort_by: SortKey,
